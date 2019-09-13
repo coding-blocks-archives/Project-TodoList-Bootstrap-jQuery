f4ce25e1712b0b4a3f07d176a92d8cc6c38ecbbd
Arnav Gupta <arnav@codingblocks.com>
2019-09-13 19:56:12 +0530

    handle enter button events
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask = $('#inpNewTask')
 
-btnAdd.click(() => {
+function addItem() {
   let listItem = $('<li>', {
     'class': 'list-group-item',
     text: inpNewTask.val()
@@ -13,6 +13,13 @@ btnAdd.click(() => {
   })
   ulTasks.append(listItem)
   inpNewTask.val('')
+}
+
+inpNewTask.keypress((e) => {
+  if (e.which == 13) addItem()
 })
+btnAdd.click(addItem)
+
+
 
 btnClear.click(() => inpNewTask.val(''))
