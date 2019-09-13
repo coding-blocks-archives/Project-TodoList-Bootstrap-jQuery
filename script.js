0f031e9b3ec43e7dae61b928acbcdaca09352c7a
Arnav Gupta <arnav@codingblocks.com>
2019-09-13 23:03:51 +0530

    dynamic buttons
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- script.js
+++ script.js
@@ -15,20 +15,34 @@ function addItem() {
   })
   ulTasks.append(listItem)
   inpNewTask.val('')
+  toggleInputButtons()
 }
 
 function clearDone() {
   $('#ulTasks .done').remove()
+  toggleInputButtons()
 }
 
 function sortTasks() {
   $('#ulTasks .done').appendTo(ulTasks)
 }
 
+function toggleInputButtons() {
+  btnReset.prop('disabled', inpNewTask.val() == '')
+  btnAdd.prop('disabled', inpNewTask.val() == '')
+  btnSort.prop('disabled', ulTasks.children().length < 1)
+  btnCleanup.prop('disabled', ulTasks.children().length < 1)
+}
+
 inpNewTask.keypress((e) => {
   if (e.which == 13) addItem()
 })
+inpNewTask.on('input', toggleInputButtons)
+
 btnAdd.click(addItem)
-btnReset.click(() => inpNewTask.val(''))
+btnReset.click(() => {
+  inpNewTask.val('')
+  toggleInputButtons()
+})
 btnCleanup.click(clearDone)
 btnSort.click(sortTasks)
